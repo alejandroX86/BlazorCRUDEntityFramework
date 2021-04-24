52ff5f9f29c4ca74c5cda6f4c9e7a88f57fbc975
Omar Alejandro Bazar <avrmicrorobot@hotmail.com>
2021-04-24 22:20:51 +0200

    Agregar archivos de proyecto.

---
+++ Data/IBookService.cs
@@ -0,0 +1,23 @@
+﻿using BlazorCRUDEntityFramework.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorCRUDEntityFramework.Data
+{
+   public interface IBookService
+    {
+
+        Task<IEnumerable<Book>> GetAllBook();
+        Task<Book> GetBookDetails(int id);
+
+        Task<bool> InsertBook(Book book);
+
+        Task<bool> UpdateBook(Book book);
+
+        Task<bool> DeleteBook(int id);
+
+        Task<bool> SaveBook(Book book);
+    }
+}
---
+++ Models/MyBooksContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorCRUDEntityFramework.Models
+{
+    public class MyBooksContext : DbContext
+    {
+    
+     public DbSet<Book> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder  optionsBuilder) 
+        {
+
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-UCKHD7C\\SQLEXPRESS01;Initial Catalog=BlazorCRUDEntityFramework;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+
+
+        }
+
+    }
+}
